11b48eb8479c59b023cbb59ef49fc7dcc87879ce
Andrew Kozempel <itzamk@gmail.com>
2023-10-03 10:27:54 -0400

    lab4

--- lab4.py
+++ lab4.py
@@ -7,13 +7,20 @@ Fall 2023
 VIDEO
 '''
 
+import sys
+import timeit
+import tracemalloc
+
 ############################################################
 #  PART 1 - Binary Search Guesser
 ############################################################
 
 # Best: O(1) - mid element
 # Worst: O(logn) - first or last
-def binary_search(lst):
+def binsearch_guess(lst):
+
+    # print length of list
+    print("Guessing your number from list of length", len(lst))
 
     # assign left and right indices
     left = 0
@@ -25,12 +32,23 @@ def binary_search(lst):
         # calculate mid index
         mid = (left + right) // 2
 
-        # answer to less than or greater than
-        answer = int(input(f'Is your number less than (1), greater than (2), or equal to (3) {lst[mid]}?'))
+        # print memory usage
+        print(f"\nMemory usage of variable 'lst': {sys.getsizeof(lst[left:right])} bytes")
+        print(f"Memory usage of variable 'low': {sys.getsizeof(left)} bytes")
+        print(f"Memory usage of variable 'high': {sys.getsizeof(right)} bytes")
+        print(f"Memory usage of variable 'mid': {sys.getsizeof(mid)} bytes")
+
+        # initialize answer to guess
+        answer = 0
+
+        # loop to get a valid user input
+        while answer not in [1,2,3]:
+        
+            answer = int(input(f'Is your number less than (1), greater than (2), or equal to (3) {lst[mid]}? '))
 
         # if mid index is target, return index
         if answer == 3:
-            print(f'Your number is {lst[mid]}.')
+            print(f'\nYour number is {lst[mid]}.')
             return lst[mid]
         
         # search right half if greater than guess
@@ -41,40 +59,69 @@ def binary_search(lst):
         else:
             right = mid - 1
 
+# generate list for binary search
 def generate_ints(quantity, int_list):
     
     # loop "quantity" times
-    for x in range(quantity):
+    for x in range(1, quantity+1):
 
         int_list.append(x) # fill list
 
 test_list = []
-
 generate_ints(10000, test_list)
 
-test_list.sort()
-print(len(test_list))
-binary_search(test_list)
+# perform guessing game
+binsearch_guess(test_list)
 
 ############################################################
 #  PART 2 - STUDENT SORTING
 ############################################################
 
-'''
-# Best: O(1)
-# Worst: O(n)
-def student_linear_search(lst, target, key):
+# function to measure time of functions
+# *args used to pass variable number of arguments
+def measure_time(function, *args, number=1):
 
-    # iterate through list
-    for i, student in enumerate(lst):
+    wrapped = lambda: function(*args)  # Wrap the function call in a lambda to handle the arguments
+    return timeit.timeit(wrapped, number=number)* 1e9
 
-        # if element is target, return index
-        if student[key] == target:
-            return i
+# function to read students file and store as list of dictionaries
+def read_students_from_file(filename):
+
+    # initialize list
+    students = []
+    
+    # read from file
+    with open(filename, "r") as file:
+
+        # for each line, split info and store info for each student
+        for line in file:
+            student_id, first_name, last_name, email_id, major = line.split()
+            students.append({
+                "Student_ID": int(student_id),
+                "first_name": first_name,
+                "last_name": last_name,
+                "email_id": email_id,
+                "major": major
+            })
+    
+    return students
+
+# function to write the sorted list to new file
+def write_sorted_to_file(sorted_list, filename):
+    with open(filename, 'w') as file:
+
+        # write the info for each student on a new line
+        for student in sorted_list:
+            line = f"{student['Student_ID']} {student['first_name']} {student['last_name']} {student['email_id']} {student['major']}\n"
+            file.write(line)
 
 # Best: O(n)
 # Worst: O(n^2)
-def student_insertion_sort(lst, key):
+def insertion_sort(lst, key, filename):
+
+    # start tracing memory allocations
+    tracemalloc.clear_traces() # reset memory allocation
+    tracemalloc.start()
 
     # loop through list starting at second
     for i in range(1, len(lst)):
@@ -91,12 +138,28 @@ def student_insertion_sort(lst, key):
 
         # insert current element in correct spot
         lst[j + 1] = curr
+
+    # get current and peak mem allocation
+    current, peak = tracemalloc.get_traced_memory()
+    
+    # stop tracing memory allocations
+    tracemalloc.stop()
+
+    # print the memory usage
+    print(f"Memory used during sorting: {current} bytes")
+    print(f"Peak memory used during sorting: {peak} bytes")
+
+    write_sorted_to_file(lst, filename)
     
     return lst
 
 # Best: O(n^2)
 # Worst: O(n^2)
-def student_selection_sort(lst, key):
+def selection_sort(lst, key, filename):
+
+    # start tracing memory allocations
+    tracemalloc.clear_traces() # reset memory allocation
+    tracemalloc.start()
 
     # iterate through list
     for i in range(len(lst)):
@@ -113,12 +176,28 @@ def student_selection_sort(lst, key):
 
         # swap
         lst[i], lst[min_idx] = lst[min_idx], lst[i]
+
+    # get current and peak mem allocation
+    current, peak = tracemalloc.get_traced_memory()
+    
+    # stop tracing memory allocations
+    tracemalloc.stop()
+
+    # print the memory usage
+    print(f"Memory used during sorting: {current} bytes")
+    print(f"Peak memory used during sorting: {peak} bytes")
+
+    write_sorted_to_file(lst, filename)
     
     return lst
 
 # Best: O(n)
 # Worst: O(n^2)
-def student_bubble_sort(lst, key):
+def bubble_sort(lst, key, filename):
+
+    # start tracing memory allocations
+    tracemalloc.clear_traces() # reset memory allocation
+    tracemalloc.start()
 
     # length of list
     n = len(lst)
@@ -141,33 +220,114 @@ def student_bubble_sort(lst, key):
         if not swapped:
             break
 
+    # get current and peak mem allocation
+    current, peak = tracemalloc.get_traced_memory()
+    
+    # stop tracing memory allocations
+    tracemalloc.stop()
+
+    # print the memory usage
+    print(f"Memory used during sorting: {current} bytes")
+    print(f"Peak memory used during sorting: {peak} bytes")
+
+    write_sorted_to_file(lst, filename)
+
     return lst
 
-# Read file
-def read_student_data(filename):
-    # store student data
-    students = []
-    # open the file in read mode
-    with open(filename, 'r') as file:
-        # read first line to get the headers/column names
-        headers = file.readline().strip().split(',')
-        
-        # loop through each line in the file
-        for line in file:
-            # split the line into individual data items
-            data = line.strip().split(',')
-            
-            # create a dictionary for each student using a dictionary comprehension
-            student = {}
-            
-            # iterate through items in line
-            for i in range(len(headers)):
-                student[headers[i]] = data[i]
-            
-            # append the student dictionary to the students list
-            students.append(student)
+# Best: O(nlogn)
+# Worst: O(nlogn)
+def _merge_sort(lst, key):
     
-    # return the list of student dictionaries
-    return students
+    # base case: return list if 1 or 0
+    if len(lst) <= 1:
+        return lst
+    
+    # split in half
+    middle = len(lst) // 2
+    left_half = lst[:middle]
+    right_half = lst[middle:]
+    
+    # recursively sort and merge the halves
+    return merge(_merge_sort(left_half, key), _merge_sort(right_half, key), key)
 
-students_data = read_student_data('students.txt')'''
+# merge portion
+def merge(left, right, key):
+    
+    # create temp merge
+    merged = []
+
+    # initialize indices
+    left_idx, right_idx = 0, 0
+
+    # while both halves are not empty
+    while left_idx < len(left) and right_idx < len(right):
+
+        # add from left half if element is smaller
+        if left[left_idx][key] < right[right_idx][key]:
+            merged.append(left[left_idx])
+            left_idx += 1
+            
+        # add from right half if element is smaller
+        else:
+            merged.append(right[right_idx])
+            right_idx += 1
+    
+    # add extra elements from either list, if any
+    return merged + left[left_idx:] + right[right_idx:]
+
+def merge_sort(lst, key, filename):
+
+    # start tracing memory allocations
+    tracemalloc.clear_traces() # reset memory allocation
+    tracemalloc.start()
+
+    # perform merge sort
+    lst = _merge_sort(lst, key)
+
+    # get current and peak mem allocation
+    current, peak = tracemalloc.get_traced_memory()
+    
+    # stop tracing memory allocations
+    tracemalloc.stop()
+
+    # print the memory usage
+    print(f"Memory used during sorting: {current} bytes")
+    print(f"Peak memory used during sorting: {peak} bytes")
+
+    write_sorted_to_file(lst, filename)
+
+    return lst
+
+students = read_students_from_file("students.txt")
+
+# sort by ID
+
+print('\n---SORTING BY ID---')
+
+print('\nINSERTION SORT:')
+print(f"Time Taken: {measure_time(insertion_sort, students.copy(), 'Student_ID', 'insertion_id.txt'):.2f} nanoseconds")
+
+print('\nSELECTION SORT:')
+print(f"Time Taken: {measure_time(selection_sort, students.copy(), 'Student_ID','selection_id.txt'):.2f} nanoseconds")
+
+print('\nBUBBLE SORT:')
+print(f"Time Taken: {measure_time(bubble_sort, students.copy(), 'Student_ID','bubble_id.txt'):.2f} nanoseconds")
+
+print('\nMERGE SORT:')
+print(f"Time Taken: {measure_time(merge_sort, students.copy(), 'Student_ID','merge_id.txt'):.2f} nanoseconds")
+
+# sort by first name
+
+print('\n---SORTING BY FIRST NAME---')
+
+print('\nINSERTION SORT:')
+print(f"Time Taken: {measure_time(insertion_sort, students.copy(), 'first_name', 'insertion_fn.txt'):.2f} nanoseconds")
+
+print('\nSELECTION SORT:')
+print(f"Time Taken: {measure_time(selection_sort, students.copy(), 'first_name','selection_fn.txt'):.2f} nanoseconds")
+
+print('\nBUBBLE SORT:')
+print(f"Time Taken: {measure_time(bubble_sort, students.copy(), 'first_name','bubble_fn.txt'):.2f} nanoseconds")
+
+print('\nMERGE SORT:')
+print(f"Time Taken: {measure_time(merge_sort, students.copy(), 'first_name','merge_fn.txt'):.2f} nanoseconds")
